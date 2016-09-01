186a1b1da944660d757783363e00130f1d3a5b45
hiayang <zhy20090912@gmail.com>
2016-09-01 17:31:42 +0800

    add folders

--- download.sh
+++ download.sh
@@ -3,7 +3,7 @@ filename="url.txt"
 while read -r line
 do
     name="$line"
-		if youtube-dl -o '%(title)s.%(ext)s' $name; then
+		if youtube-dl -o '/home/hiayang/share/%(title)s.%(ext)s' $name; then
 				ntfy send "download succeed"
 		else
 				ntfy send "download failed"
